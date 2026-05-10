939f9b8f8cb40ec316fde466357b0715fb9ede5b
sofiaricopinto <soysofiarico@gmail.com>
2026-05-10 13:25:07 +0200

    Arreglar redireccion de Netlify Identity

---
+++ assets/js/netlify-identity-redirect.js
@@ -0,0 +1,26 @@
+(function () {
+    const identityTokenPattern = /(invite_token|confirmation_token|recovery_token|email_change_token|access_token)=/;
+    const identityPayload = `${window.location.search || ""}${window.location.hash || ""}`;
+    const isAdmin = window.location.pathname.replace(/\/+$/, "").endsWith("/admin");
+
+    if (!isAdmin && identityTokenPattern.test(identityPayload)) {
+        window.location.replace(`/admin/${window.location.search || ""}${window.location.hash || ""}`);
+        return;
+    }
+
+    if (!window.netlifyIdentity) return;
+
+    window.netlifyIdentity.on("init", () => {
+        if (identityTokenPattern.test(identityPayload)) {
+            window.netlifyIdentity.open();
+        }
+    });
+
+    window.netlifyIdentity.on("login", () => {
+        if (isAdmin) {
+            window.location.reload();
+            return;
+        }
+        window.location.href = "/admin/";
+    });
+}());
